1a62876d56ea63dc4cb91d10974fc14a10b8044c
Jesper Kristensen <cableman@linuxdev.dk>
2023-12-23 11:39:32 +0100

    Minor ajustments in hour off-set

--- clock-led-ring.ino
+++ clock-led-ring.ino
@@ -8,6 +8,8 @@
 
 CRGB leds[NUM_LEDS];
 
+int brightness = 80;
+int hourSteps = NUM_LEDS/12;
 bool runTest = true;
 
 /**
@@ -30,7 +32,8 @@ void simpleTime()
   FastLED.clear();
 
   String hour = myTZ.dateTime("h");
-  leds[hour.toInt()*5] = CRGB::Green;
+  int current = hour.toInt()*hourSteps;
+  leds[current ? current -1 : 0] = CRGB::Green;
 
   String min = myTZ.dateTime("i");
   leds[min.toInt()] = CRGB::Blue;
@@ -42,10 +45,13 @@ void simpleTime()
   FastLED.delay(1000);
 }
 
+/**
+ * Test that leds are working.
+ */
 void ledTest() {
-  for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+  for(int currentLed = 0; currentLed < NUM_LEDS; currentLed = currentLed + 1) {
       // Turn our current led on to white, then show the leds
-      leds[whiteLed] = CRGB::Blue;
+      leds[currentLed] = CRGB::Blue;
  
       // Show the leds (only one of which is set to white, from above)
       FastLED.show();
@@ -54,16 +60,19 @@ void ledTest() {
       FastLED.delay(100);
  
       // Turn our current led back to black for the next loop around
-      leds[whiteLed] = CRGB::Black;
+      leds[currentLed] = CRGB::Black;
    }
 }
 
+/**
+ * Setup leds, connect to wifi and get NTP time.
+ */
 void setup() {
   // sanity check delay - allows reprogramming if accidently blowing power w/leds
   delay(100);
 
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(40);
+  FastLED.setBrightness(brightness);
 
   // Connect to WIFI.
   MyWifi* myWifi = new MyWifi(leds);
@@ -73,8 +82,12 @@ void setup() {
   connectTime();
 }
 
+/**
+ * Run the show.
+ */
 void loop() {
   if (runTest) {
+    // Run led test the first time to light up evert led (check that they work).
     ledTest();
     runTest = false;
   }
